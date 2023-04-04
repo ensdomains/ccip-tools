0d4abbea98a93cfd34edd10a2f7e459c23fc7aa4
Lucemans <luc@lucemans.nl>
2023-04-04 17:45:30 +0000

    Initial Commit

---
+++ web/next.config.js
@@ -0,0 +1,12 @@
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+    output: 'export',
+    distDir: 'dist',
+    experimental: {
+        appDir: true,
+    },
+};
+
+module.exports = nextConfig;
