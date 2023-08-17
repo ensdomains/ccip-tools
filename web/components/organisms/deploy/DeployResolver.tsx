5b020d335acfaf06c6a875f5b2140f065aececcc
Greg <35093316+gskril@users.noreply.github.com>
2023-08-17 10:15:01 -0400

    Handle transaction state, remove broken links (#1)
    * Add buttons for tx processing/success states
    
    * Temporarily remove broken links
    
    just for short term sharability - I know these will be filled in later
    
    * Update meta tags

--- web/components/organisms/deploy/DeployResolver.tsx
+++ web/components/organisms/deploy/DeployResolver.tsx
@@ -1,8 +1,8 @@
-import { Button, Card, FlameSVG, GasPumpSVG, Input, WalletSVG } from "@ensdomains/thorin";
+import { Button, Card, FlameSVG, GasPumpSVG, Input, OutlinkSVG, WalletSVG } from "@ensdomains/thorin";
 import { useChains, useModal } from "connectkit";
 import { FactoryABI } from "../../../pages/abi/factory_abi";
 import { FC, useMemo, useState } from "react";
-import { Address, useAccount, useChainId, useConnect, useContractWrite, useFeeData, usePrepareContractWrite } from "wagmi";
+import { Address, useAccount, useChainId, useContractWrite, useFeeData, usePrepareContractWrite, useWaitForTransaction } from "wagmi";
 import { formatEther } from "viem";
 
 // https url that must include '{sender}'
@@ -53,7 +53,8 @@ export const DeployResolverCard: FC = () => {
         abi: FactoryABI,
         enabled: isReady,
     });
-    const { write, data } = useContractWrite(config as any);
+    const { write, data } = useContractWrite(config);
+    const receipt = useWaitForTransaction(data);
 
     const gas = useMemo(() => {
         if (!EstimateData) return null;
@@ -73,8 +74,9 @@ export const DeployResolverCard: FC = () => {
         <Card className="leading-6 gap-2">
             <div className="">
                 <h2 className="font-bold">Deploy an Offchain Resolver</h2>
-                <p className="text-neutral-700">
-                    In order to ensure you get these settings right, please read <a className="link">this page</a>.
+                <p className="text-neutral-700 mt-2">
+                    In order to use a gateway, you need to deploy a resolver to mainnet.
+                    We will help you deploy your <a className="link" href="https://github.com/ensdomains/ccip-tools/blob/master/contracts/OffchainResolver.sol">OffchainResolver</a>.
                 </p>
             </div>
 
@@ -111,7 +113,7 @@ export const DeployResolverCard: FC = () => {
             {
                 error && (
                     <p className="text-red-500">
-                        {JSON.stringify(error)}
+                        {chainId !== 5 ? 'Only Goerli is supported right now' : error.message}
                     </p>
                 )
             }
@@ -133,20 +135,21 @@ export const DeployResolverCard: FC = () => {
                 )
             }
             {
-                isConnected && (
-                    <Button disabled={!isReady || !write} onClick={() => {
-                        write?.();
-                    }}>
-                        {
-                            (() => {
-                                if (isLoading) return "Estimating Fees...";
-                                if (isSuccess) return "Deploy " + EstimateData?.request.gasLimit + " gas";
+                (() => {
+                    if (!isConnected) return null;
 
-                                return "Deploy";
-                            })()
-                        }
-                    </Button>
-                )
+                    if (receipt.isSuccess) return (
+                        <Button colorStyle="greenPrimary" suffix={<OutlinkSVG />} as="a" target="_blank" href={
+                            `https://${chainId === 5 ? "goerli." : ""}etherscan.io/tx/${receipt.data?.transactionHash}#internal`
+                        }>View on Etherscan</Button>
+                    );
+
+                    return (
+                        <Button disabled={!isReady || !write || receipt.isLoading} loading={receipt.isLoading} onClick={() => write?.()}>
+                            {receipt.isLoading ? "Processing" : isLoading ? 'Estimating Fees...' : isSuccess ? 'Deploy ' + EstimateData?.request.gasLimit + ' gas' : 'Deploy'}
+                        </Button>
+                    );
+                })()
             }
         </Card>
     )
